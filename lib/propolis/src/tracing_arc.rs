d051926c3129cfb8d06c73a0489660100d1e66dc
Greg Colombo <greg@oxidecomputer.com>
2025-04-02 23:04:09 +0000

    add ref counts to tracing arc probes

--- lib/propolis/src/tracing_arc.rs
+++ lib/propolis/src/tracing_arc.rs
@@ -1,32 +1,40 @@
 use std::ops::{Deref, DerefMut};
 use std::sync::Arc;
 
-pub struct TracingArc<T>(Arc<T>);
+pub struct TracingArc<T>(pub Arc<T>);
 
 #[usdt::provider(provider = "propolis")]
 mod probes {
-    fn arc_acquire(addr: u64) {}
-    fn arc_release(addr: u64) {}
+    fn arc_acquire(addr: u64, new_count: u64) {}
+    fn arc_release(addr: u64, new_count: u64) {}
 }
 
 impl<T> TracingArc<T> {
     pub fn new(data: T) -> Self {
         let this = Arc::new(data);
-        probes::arc_acquire!(|| (std::sync::Arc::as_ptr(&this) as u64));
+        probes::arc_acquire!(|| (std::sync::Arc::as_ptr(&this) as u64, 1u64));
         Self(this)
     }
 }
 
 impl<T> Clone for TracingArc<T> {
     fn clone(&self) -> Self {
-        probes::arc_acquire!(|| (std::sync::Arc::as_ptr(&self.0) as u64));
+        let count = std::sync::Arc::strong_count(&self.0) as u64;
+        probes::arc_acquire!(|| (
+            std::sync::Arc::as_ptr(&self.0) as u64,
+            count + 1
+        ));
         Self(self.0.clone())
     }
 }
 
 impl<T> Drop for TracingArc<T> {
     fn drop(&mut self) {
-        probes::arc_release!(|| (std::sync::Arc::as_ptr(&self.0) as u64));
+        let count = std::sync::Arc::strong_count(&self.0) as u64;
+        probes::arc_release!(|| (
+            std::sync::Arc::as_ptr(&self.0) as u64,
+            count - 1
+        ));
     }
 }
 
